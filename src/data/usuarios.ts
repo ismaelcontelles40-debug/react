a100c9d9b0b9f00d1e77afe9d8c2822407274b3b
ismael <ismaelcontelles40@gmail.com>
2026-05-18 13:56:20 +0200

    finalizar practica react typescript

--- src/data/usuarios.ts
+++ src/data/usuarios.ts
@@ -4,21 +4,21 @@ export const usuarios: Usuario[] = [
   {
     id: "USR-001",
     nombre: "Ismael Contelles",
-    email: "ismael@example.com",
+    email: "ismael.contelles@campusfp.es",
     rol: "ESTUDIANTE",
     activo: true
   },
   {
     id: "USR-002",
     nombre: "Laura Martínez",
-    email: "laura@example.com",
+    email: "laura.martinez@empresa.dev",
     rol: "PROFESOR",
     activo: true
   },
   {
     id: "USR-003",
     nombre: "Carlos Ruiz",
-    email: "carlos@example.com",
+    email: "carlos.ruiz@adminpanel.dev",
     rol: "ADMIN",
     activo: false
   }
